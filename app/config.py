e44d8aea4f044f77a936524c278655270cb64af8
Theideabased <ogunmusireseyi01@gmail.com>
2025-01-28 17:01:18 +0100

    removing the unnecessary in the .env and config file

--- app/config.py
+++ app/config.py
@@ -6,8 +6,6 @@ class Settings(BaseSettings):
     AUTH0_CLIENT_ID: str
     AUTH0_CLIENT_SECRET: str
     AUTH0_API_AUDIENCE: str
-    AUTH0_REDIRECT_URI: str
-    AUTH0_LOGOUT_REDIRECT_URI: str
     APP_SECRET_KEY: str
 
     class Config: 
